0d885b42dedead96414b0179eb8b2b648bef4ad1
Rei <Zero4one@yandex.ru>
2023-10-05 14:57:40 +0500

    Не доделал jS лр 1

---
+++ JS_Lab1/myScript.js
@@ -0,0 +1,12 @@
+console.log("Этот текст вы увидите в консоли")
+
+//var firstNumber = prompt("Введите первое число");
+//var secondNumber = prompt("Введите второе число");
+//var total = Number(firstNumber) + Number(secondNumber);
+//alert(total)
+
+// остановился на конце 16 страницы
+
+
+
+
